ea250ea966f9bcce2efb9f01736284f1dd30a511
Steve Gray <sjgray@rogers.com>
2021-06-19 21:26:48 -0400

    CBM2-V1-R1
    Add CBM-II Keyboard, V1-R1. This design is complete,. The PCB mounts
    directly to the case and all keys are functional. It uses a 6.25U
    spacebar.

---
+++ CBM2-V1-R1/gerbers/rename.bat
@@ -0,0 +1,7 @@
+ren cbm2mx.drl           cbm2mx.txt
+ren cbm2mx-B_Cu.gbr      cbm2mx.gbl
+ren cbm2mx-B_Mask.gbr    cbm2mx.gbs
+ren cbm2mx-Edge_Cuts.gbr cbm2mx.gko
+ren cbm2mx-F_Cu.gbr      cbm2mx.gtl
+ren cbm2mx-F_Mask.gbr    cbm2mx.gts
+ren cbm2mx-F_SilkS.gbr   cbm2mx.gto
